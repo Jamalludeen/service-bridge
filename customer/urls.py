2cedacdd10afa9e0ce55fd68f0da1ca585648795
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-29 20:15:19 +0430

    Removed extra endpoints

--- customer/urls.py
+++ customer/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path("profile/", views.CustomerProfileView.as_view(), name="profile"),
-    # path("my-profile/", views.CustomerProfileRetrieveView.as_view(), name="get_customer_profile"),
-    # path("update-profile/", views.CustomerProfileUpdateView.as_view(), name="update_customer_profile"),
 ]
