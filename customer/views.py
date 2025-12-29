9f447fba057ecdd66b6ff9869952c32991bc71db
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-29 20:09:01 +0430

    customers view update

--- customer/views.py
+++ customer/views.py
@@ -1,15 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import RetrieveAPIView
 
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
-from .serializers import CustomerProfileSerializer, CustomerRetrieveProfileSerializer, CustomerUpdateProfileSerializer
-from .permissions import IsCustomerOwner, IsAdmin
+from .serializers import CustomerProfileSerializer, CustomerUpdateProfileSerializer
+from .permissions import IsCustomerOwner
 from .models import CustomerProfile
 
 User = get_user_model()
