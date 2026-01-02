e6896f02c3857989b8815fa3d1c8481fc924eb20
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-02 14:20:16 +0430

    Add OTP verification rate limit class

--- core/throttles.py
+++ core/throttles.py
@@ -3,4 +3,8 @@ from rest_framework.throttling import UserRateThrottle
 
 class UserAuthThrottle(UserRateThrottle):
     rate = "10/min"
-    
+
+
+class OTPVerifyThrottle(UserRateThrottle):
+    rate = "1/min"
+
