6f245b1175847fa017f5382991715d5ac6c86535
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-14 14:21:30 +0430

    Add IsAdminUser permission class

--- service/permissions.py
+++ service/permissions.py
@@ -1,5 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == "admin"
+
 class IsProfessionalOwnerOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
