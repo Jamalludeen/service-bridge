30c138b60d72ad0148a770ea975cb5c7489092cb
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-16 09:29:09 +0430

    Implement additional permission classes

--- booking/permissions.py
+++ booking/permissions.py
@@ -24,3 +24,44 @@ class IsBookingParticipant(BasePermission):
         )
     
 
+
+class CanAcceptBooking(BasePermission):
+    """Professional can accept only PENDING bookings"""
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.professional.user == request.user and
+            obj.status == 'PENDING'
+        )
+    
+
+class CanStartBooking(BasePermission):
+    """Professional can start only ACCEPTED bookings"""
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.professional.user == request.user and
+            obj.status == 'ACCEPTED'
+        )
+    
+
+class CanCompleteBooking(BasePermission):
+    """Professional can complete only IN_PROGREE bokkings"""
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.professional.user == request.user and 
+            obj.status == 'IN_PROGRESS'
+        )
+    
+
+class CanCancelBooking(BasePermission):
+    """Either party can cancel if not completed"""
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        is_participant = (
+            obj.customer.user == user or
+            obj.professional.user == user
+        )
+        can_cancel = obj.status in ['PENDING', 'ACCEPTED']
+        return is_participant and can_cancel
+
+
+    
