9b55cac02dab3e7838fab36770928097a4609f5e
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-16 09:36:54 +0430

    Added more logic to BookingViewSet

--- booking/views.py
+++ booking/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Booking, BookingStatusHistory
 from .serializers import (
@@ -10,6 +12,15 @@ from .serializers import (
     BookingListSerializer,
     BookingDetailSerializer,
     BookingStatusUpdateSerializer
+)
+from .permissions import (
+    IsBookingCustomer,
+    IsBookingProfessional,
+    IsBookingParticipant,
+    CanAcceptBooking,
+    CanStartBooking,
+    CanCompleteBooking,
+    CanCancelBooking
 )
 
 
@@ -45,4 +56,43 @@ class BookingViewSet(ModelViewSet):
             return BookingListSerializer
         return BookingDetailSerializer()
     
+    def get_permissions(self):
+        if self.action == 'create':
+            # only customers can create bookings
+            return [IsAuthenticated()]
+        elif self.action is ['retrieve', 'list']:
+            return [IsAuthenticated(), IsBookingParticipant]
+        return super().get_permissions()
+
+
+    def create(self, request, *args, **kwargs):
+        if request.user.role != 'customer':
+            return Response(
+                {"error": "Only customers can create bookings."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
+        return super().create(request, *args, **kwargs)
+
+    def _update_status(self, booking, new_status, user, note='', **extra_fields):
+        """Helper to update status with history tracking"""
+        old_status = booking.status
+        booking.status = new_status
+
+        for field, value in extra_fields.items():
+            setattr(booking, field, value)
+        booking.save()
+
+        BookingStatusHistory.objects.create(
+            booking=booking,
+            from_status=old_status,
+            to_status=new_status,
+            changed_by=user,
+            note=note
+        )
+
+        return booking
     
+    
+    
+
