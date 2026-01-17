69990afda318f3007c7f3626403bf860dbe3d9a9
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-17 19:01:15 +0430

    ImportError fixed

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import BookingViewSet, BookingStatusHistoryViewSet
+from .views import BookingViewSet
 
 router = DefaultRouter()
 router.register('', BookingViewSet, basename='booking')
