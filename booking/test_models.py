d63e1e1c651e095e03a3234e1100108ff7c32bbc
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-21 15:29:55 +0430

    add booking creation test

--- booking/test_models.py
+++ booking/test_models.py
@@ -3,3 +3,13 @@ from decimal import Decimal
 from datetime import date, time
 from booking.models import Booking
 
+@pytest.mark.django_db
+def test_booking_creation(booking):
+    assert booking.id is not None
+    assert booking.status == 'PENDING'
+    assert booking.customer is not None
+    assert booking.professional is not None
+    assert booking.service is not None
+    assert booking.estimated_price == Decimal('500.00')
+
+
