71fe6791d0428e5860f81316c2809e25af10bd41
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-21 14:47:43 +0430

    add fixtures for roles

---
+++ conftest.py
@@ -0,0 +1,49 @@
+import pytest
+from rest_framework.test import APIClient
+from core.models import User
+from customer.models import CustomerProfile
+from professional.models import Professional
+from booking.models import Booking
+from payment.models import Payment
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        username='testcustomer',
+        email='customer@gmail.com',
+        password='TestPass123',
+        phone='+93700000001',
+        role='customer',
+        is_verified=True
+    )
+
+@pytest.fixture
+def professional_user(db):
+    return User.objects.create_user(
+        username='testprofessional',
+        email='pro@gmail.com',
+        password='testpAss123',
+        phone='+93700000002',
+        role='professional',
+        is_verified=True
+    )
+
+@pytest.fixture
+def admin_user(db):
+    """Creates an admin user."""
+    return User.objects.create_user(
+        username='testadmin',
+        email='admin@test.com',
+        password='testpass123',
+        phone='+93700000003',
+        role='admin',
+        is_staff=True,
+        is_superuser=True,
+        is_verified=True
+    )
