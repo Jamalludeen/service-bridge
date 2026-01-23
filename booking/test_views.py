db97374fb40224249adeebc8d0b49d997bf3f0e8
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-23 13:40:20 +0430

    add create booking as customer view test

--- booking/test_views.py
+++ booking/test_views.py
@@ -18,14 +18,12 @@ def test_bookings_list_with_authenticated_user(authenticated_client):
 
     assert response.status_code == status.HTTP_200_OK
 
-
 @pytest.mark.django_db
 def test_booking_retrieve_with_unauthenticated_user(api_client, booking):
     urls = reverse('booking-detail', args=[booking.id])
     response = api_client.get(urls)
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
 
 @pytest.mark.django_db
 def test_booking_retrieve_with_authenticated_user(authenticated_client, booking):
@@ -35,3 +33,16 @@ def test_booking_retrieve_with_authenticated_user(authenticated_client, booking)
 
     assert response.data.get('id') == booking.id
 
+@pytest.mark.django_db
+def test_create_booking_as_customer(authenticated_client, service, customer_profile, professional):
+    url = reverse('booking-list')
+    data = {
+        'service_id': service.id,
+        'scheduled_date': '2026-02-01',
+        'scheduled_time': '10:00:00',
+        'address': '123 Main St',
+        'city': 'Metropolis',
+        'estimated_price': '150.00',
+    }
+    response = authenticated_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
