baccd4a2cccfc13ee22e8062d0a4efee3235c32b
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-26 20:46:11 +0430

    add new serializer for ReviewListSerializer

--- review/serializers.py
+++ review/serializers.py
@@ -24,3 +24,37 @@ class ReviewResponseSerializer(serializers.ModelSerializer):
             'updated_at'
         ]
         read_only_fields = ['professional', 'created_at', 'updated_at']
+
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    """Serializer for listing reviews"""
+
+    customer_name = serializers.SerializerMethodField()
+    service_title = serializers.CharField(
+        source='booking.service.title',
+        read_only=True
+    )
+    response = ReviewResponseSerializer(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = [
+            'id',
+            'booking',
+            'customer_name',
+            'service_title',
+            'rating',
+            'comment',
+            'response',
+            'created_at',
+            'updated_at'
+        ]
+
+    def get_customer_name(self, obj):
+        """Return customer first name + last initial if available, else username"""
+        user = obj.customer
+        full_name = user.get_full_name()
+
+        if full_name:
+            return full_name
+        return user.username
