9e037efba16206fb86eca327fbc141e2e6946c92
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-01 13:58:51 +0430

    implement customer registration test

--- core/test_views.py
+++ core/test_views.py
@@ -1,11 +1,11 @@
-import pytest
-from django.urls import reverse
 from rest_framework import status
+from django.urls import reverse
+import pytest
 
 
 
 @pytest.mark.django_db
-def test_login_with_valid_password(api_client, user):
+def test_login_with_valid_credentials(api_client, user):
     url = reverse('login')
     data = {
         'email': user.email,
@@ -13,9 +13,9 @@ def test_login_with_valid_password(api_client, user):
     }
     response = api_client.post(url, data=data, format='json')
     assert response.status_code == status.HTTP_200_OK
-    
+
 @pytest.mark.django_db
-def test_login_with_invalid_password(api_client, user):
+def test_login_with_invalid_credentials(api_client, user):
     url = reverse('login')
     data = {
         'email': user.email,
@@ -23,3 +23,15 @@ def test_login_with_invalid_password(api_client, user):
     }
     response = api_client.post(url, data=data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+@pytest.mark.django_db
+def test_customer_registration_view(api_client):
+    url = reverse('customer_register')
+    data = {
+        'username': 'John.Smith',
+        'email': 'john.smith@gmail.com',
+        'phone': '+93788727887',
+        'password': 'ILoveDjango',
+    }
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
