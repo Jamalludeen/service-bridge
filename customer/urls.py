80ffdc9384dec12b1fc4487a8e3bc27ebbed90ed
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-07 14:15:31 +0430

    add URL configuration and router setup for cart endpoints

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path ,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'cart', views.CartViewSet, basename='cart')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path("profile/", views.CustomerProfileView.as_view(), name="profile"),
 ]
