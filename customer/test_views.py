e1725ac839ddea865b261fe58b8ff399bed1f63a
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-09 20:57:36 +0430

    test(customer): add test for PATCH profile city and district

--- customer/test_views.py
+++ customer/test_views.py
@@ -24,3 +24,18 @@ def test_profile_get_success(authenticated_client, customer_profile):
     assert response.status_code == status.HTTP_200_OK
     assert response.data['city'] == customer_profile.city
     assert response.data['district'] == customer_profile.district
+
+
+@pytest.mark.django_db
+def test_profile_patch_update_city_and_district(authenticated_client, customer_profile):
+    """Customer can update their city and district."""
+    url = reverse('profile')
+    data = {'city': 'Herat', 'district': '5th'}
+    response = authenticated_client.patch(url, data, format='json')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['data']['city'] == 'Herat'
+    assert response.data['data']['district'] == '5th'
+
+    customer_profile.refresh_from_db()
+    assert customer_profile.city == 'Herat'
+    assert customer_profile.district == '5th'
