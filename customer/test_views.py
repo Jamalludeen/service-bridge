693d61538e1540651ee980eba2559c75f0a5d391
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-09 20:57:07 +0430

    test(customer): add test for authenticated profile GET

--- customer/test_views.py
+++ customer/test_views.py
@@ -14,3 +14,13 @@ def test_profile_get_unauthenticated(api_client):
     url = reverse('profile')
     response = api_client.get(url)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_profile_get_success(authenticated_client, customer_profile):
+    """Authenticated customer should retrieve their profile."""
+    url = reverse('profile')
+    response = authenticated_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['city'] == customer_profile.city
+    assert response.data['district'] == customer_profile.district
